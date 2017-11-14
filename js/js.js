9378497fc961150b8a4523991f48ffa1d21dd6fd
Gabriel Dal Bó <gabrieldalbo@gmail.com>
2017-11-14 15:31:37 -0300

    1.0.2

--- js/js.js
+++ js/js.js
@@ -25,3 +25,23 @@ $(".imagen").click(function(e){
     $("#./Static/Larga/index2.jpg").elevateZoom();
     $("./Static/Larga/index4.jpg").elevateZoom();
 });
+$(document).ready(function(){ /* Cualquier funcionalidad que queramos agregar a la página por medio de jQuery, debe ser incluida cuando el documento está listo para recibir acciones que modifiquen el DOM de la página. */
+      $('.azul').click(function(event){ /* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#08c');/* La función a realizar añadir CSS al body previamente seleccionado */
+      }); 
+      $('.blanco').click(function(event){/* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#fff');/* La función a realizar añadir CSS al body previamente seleccionado */
+      }); 
+    $('.rojo').click(function(event){/* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#AE4545');/* La función a realizar añadir CSS al body previamente seleccionado */
+      });
+    $('.verde').click(function(event){/* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#20be07');/* La función a realizar añadir CSS al body previamente seleccionado */
+      });
+     $('.negro').click(function(event){/* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#0b0c0b');/* La función a realizar añadir CSS al body previamente seleccionado */
+      });
+     $('.amarillo').click(function(event){/* Seleccionamos el elemento que queremos que realice la función */ 
+         $('body').css('background', '#e0ff00');/* La función a realizar añadir CSS al body previamente seleccionado */
+      });
+}); 
